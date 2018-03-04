dd169227e0b945cf96bcedcd5cd8a99cdf4e5bc2
Klas Harrysson <klaha707@student.liu.se>
2018-03-04 22:15:47 +0100

    Auto-generated commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,38 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 //TODO - move functions somewhere else
 function _splitPair(p) {
     var ret = [];
@@ -373,7 +408,7 @@ node = {
             var head, counter, pred_block, sopbytes, returnedContracts;
             var promises = [];
             promises.push(node.query('/blocks/head'));
-            if (typeof fee != 'unfedined') {
+            if (typeof fee != 'undefined') {
                 promises.push(node.query('/blocks/prevalidation/proto/context/contracts/' + keys.pkh + '/counter'));
             }
             return Promise.all(promises).then(function (f) {
@@ -524,6 +559,9 @@ eztz = {
 };
 //Alpha only functions
 eztz.alphanet = {};
+eztz.alphanet.sleep = function (ms) {
+    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
+};
 eztz.alphanet.faucet = function (toAddress) {
     var keys = crypto.generateKeysNoSeed();
     var head, pred_block, opbytes, npkh;
@@ -556,7 +594,16 @@ eztz.alphanet.faucet = function (toAddress) {
         });
     })
         .then(function (f) {
-        return node.query('/blocks/prevalidation/proto/context/contracts/' + npkh + '/manager');
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, eztz.alphanet.sleep(500)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/, node.query('/blocks/prevalidation/proto/context/contracts/' + npkh + '/manager')];
+                }
+            });
+        });
     })
         .then(function (f) {
         //Transfer from free account
